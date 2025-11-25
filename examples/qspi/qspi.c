dfd06ab9ce40a4ddace3358b780d016a17933fcd
Kresimir Popovic <kresimir@noisesculptors.com>
2025-11-25 13:48:21 +0100

    added pll2 as clock source for qspi in the example

--- examples/qspi/qspi.c
+++ examples/qspi/qspi.c
@@ -7,7 +7,16 @@
 #include "serial.h"
 #include "user_io.h"
 
-#define QSPI_CLK_MULTIPLIER 2U /* multiplier 2, qspi clk == 80MHz */
+/* do not use 0 as QSPI_CLK_MULTIPLIER as it hangs the chip, possible described
+here, possibly hangs when using other clock soruces than QSPISEL:0
+ https://www.st.com/resource/en/errata_sheet/es0445-stm32h745xig-stm32h755xi-stm32h747xig-stm32h757xi-device-errata-stmicroelectronics.pdf
+ */
+#define QSPI_CLK_MULTIPLIER 2U /* 2: Fquadspi_ker_ck/3; 240/3= 80MHz */
+
+#if defined(QSPI_PRESCALER) && (QSPI_PRESCALER == 0)
+#error "QSPI prescaler 0 is forbidden due to STM32H7 QUADSPI errata"
+#endif
+
 #define QSPI_BASE           0x90000000UL
 #define QSPI_SIZE           (16 * 1024 * 1024UL)
 
@@ -18,7 +27,15 @@ int main(void)
     init_clock();
     init_dwt();
     user_serial_init();
+
+    /* 80MHz qspi */
     init_qspi(QSPI_CLK_MULTIPLIER);
+
+    /* or */
+
+    /* 104MHz qspi (max allowed for the flash chip), PLL2 as clock source */
+//  init_qspi_pll2_clock_source_qspi_104MHz();
+
     qspi_memory_map_mode();
 
     printf("\033[2J\033[H"); /* clear screen */
@@ -28,15 +45,15 @@ int main(void)
     uint32_t words = QSPI_SIZE / 4;
     uint32_t sum32 = 0;
 
-    dwt_start();    // your wrapper
+    dwt_start();
 
     for (uint32_t i = 0; i < words; i++) {
-        sum32 ^= p32[i];   // 32-bit reads
+        sum32 ^= p32[i];   /* 32-bit reads */
     }
 
     uint32_t cycles = dwt_stop();
 
-    // keep it so compiler doesn’t kill the loop
+    /* keep it so compiler doesn't kill the loop */
     printf("\nsum32   = 0x%08lx\n", (unsigned long)sum32);
 
     double seconds  = (double)cycles / (double)ci.sysclk_hz;
